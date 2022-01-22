c4ebf1a2d5f744efc51c127a96ca7690d61228f9
tariqulislamsagar.cse@gmail.com <tariqulislamsagar.cse@gmail.com>
2022-01-23 00:14:05 +0600

    Guess game updated

--- guessing_game.py
+++ guessing_game.py
@@ -2,9 +2,9 @@ import random
 n = random.randint(1,20)
 g_count = 0
 g_limit = 2
-num = int(input("Enter an integer between 1 to 20: "))
+num = int(input("Guess an integer between 1 to 20: "))
 while g_count < g_limit:
-    number = int(input("Sorry! Type again: "))
+    number = int(input("Sorry! Guess again: "))
     g_count +=1
     if number == n:
         print("You Won!")
